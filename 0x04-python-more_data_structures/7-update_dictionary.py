58fe1067dde6b54310e8c348b31c4d431902d393
Sarah <sarah.t.karim@gmail.com>
2023-07-19 04:43:24 -0700

    more data structures

---
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+
+def update_dictionary(a_dictionary, key, value):
+    """function to replace or add key to value pairs in a dictionary."""
+    a_dictionary[key] = value
+    return (a_dictionary)
